88a3040291962b7aff12ccd5340083196b719e04
MiguelTucto <u201521691@upc.edu.pe>
2023-09-05 20:51:03 -0500

    feat: provider component has been implemented

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import '@styles/globals.css';
 import Nav from "@components/Nav";
+import Provider from "@components/Provider";
 
 export const metadata = {
     title: "Login Project",
@@ -10,10 +11,12 @@ const RootLayout = ({children}) => {
     return (
         <html>
             <body>
-                <main className="app">
-                    <Nav />
-                    {children}
-                </main>
+                <Provider>
+                    <main className="app">
+                        <Nav />
+                        {children}
+                    </main>
+                </Provider>
             </body>
         </html>
     )
